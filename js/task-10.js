867a8bfd41ec2f4f146d796569e99aae6aec2322
Michalinius <101971811+Michalinius@users.noreply.github.com>
2022-07-19 00:57:14 +0200

    done

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,33 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const input = document.querySelector("div#controls>input");
+const createButton =document.querySelector("button[data-create]");
+const destroyButton =document.querySelector("button[data-destroy]");
+const divBoxes = document.querySelector("div#boxes");
+
+const createBoxes = (amount) => {
+  let divSize=20;
+  for (let i=0;i<amount;i++) {
+    const box = document.createElement("div")
+    divSize = divSize + 10;
+    box.style.width = divSize+"px";
+    box.style.height = divSize+"px";
+    box.style.backgroundColor=getRandomHexColor();
+    console.log(box);
+    divBoxes.append(box);
+    //divBoxes.insertAdjacentHTML("beforeend",`<div width="${divSize}px" height="${divSize}px"></div>`);
+  }
+  
+}
+
+const destroyBoxes = () => {
+  divBoxes.innerHTML="";
+}
+
+//createBoxes(3);
+createButton.addEventListener("click", createBoxes(input.value));
+destroyButton.addEventListener("click",destroyBoxes());
+
+
